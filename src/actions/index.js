a74b5df3857f18b99ee4228728936964d986e3ed
Ali Sufyan <galisufyan327@gmail.com>
2023-09-21 15:13:13 +0500

    fix: add the debounce funcationality

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,11 @@
-import axios from 'axios';
+import axios from "axios";
 
-export const OPEN_MODAL = 'OPEN_MODAL';
-export const CLOSE_MODAL = 'CLOSE_MODAL';
-export const FETCH_CONTACTS_REQUEST = 'FETCH_CONTACTS_REQUEST';
-export const FETCH_CONTACTS_SUCCESS = 'FETCH_CONTACTS_SUCCESS';
-export const FETCH_CONTACTS_FAILURE = 'FETCH_CONTACTS_FAILURE';
-export const CHECKBOX_CHECKED = 'CHECKBOX_CHECKED';
+export const OPEN_MODAL = "OPEN_MODAL";
+export const CLOSE_MODAL = "CLOSE_MODAL";
+export const FETCH_CONTACTS_REQUEST = "FETCH_CONTACTS_REQUEST";
+export const FETCH_CONTACTS_SUCCESS = "FETCH_CONTACTS_SUCCESS";
+export const FETCH_CONTACTS_FAILURE = "FETCH_CONTACTS_FAILURE";
+export const CHECKBOX_CHECKED = "CHECKBOX_CHECKED";
 
 export const fetchContactsRequest = () => ({
   type: FETCH_CONTACTS_REQUEST,
@@ -21,38 +21,48 @@ export const fetchContactsFailure = (error) => ({
   payload: error,
 });
 
-export const toggleCheckbox = (checkBoxChecked) => ({ checkBoxChecked: checkBoxChecked, type: CHECKBOX_CHECKED })
-export const openModal = (modalType) => ({ modalType: modalType, type: OPEN_MODAL });
+export const toggleCheckbox = (checkBoxChecked) => ({
+  checkBoxChecked: checkBoxChecked,
+  type: CHECKBOX_CHECKED,
+});
+export const openModal = (modalType) => ({
+  modalType: modalType,
+  type: OPEN_MODAL,
+});
 export const closeModal = () => ({ type: CLOSE_MODAL });
 export const fetchContacts = (query, page, modalType) => {
   return (dispatch) => {
     dispatch(fetchContactsRequest());
-    console.log(modalType)
-    let isLaptopsModal = modalType == 'B'
+    console.log(modalType);
+    let isLaptopsModal = modalType === "B";
 
-    let url = query == '' || isLaptopsModal
-      ? 'https://dummyjson.com/products/'
-      : 'https://dummyjson.com/products/search'
+    let url =
+      query === "" || isLaptopsModal
+        ? "https://dummyjson.com/products/"
+        : "https://dummyjson.com/products/search";
 
-    if(isLaptopsModal) {
-      url = url + `category/laptops`
-      console.log(url)
+    if (isLaptopsModal) {
+      url = url + `category/laptops`;
+      console.log(url);
     }
 
-    let params = query == '' || isLaptopsModal ? {
-      limit: 10,
-      skip: 10 * (page - 1),
-    } : {
-      q: query
-    }
+    let params =
+      query === "" || isLaptopsModal
+        ? {
+            limit: 10,
+            skip: 10 * (page - 1),
+          }
+        : {
+            q: query,
+          };
 
     axios
-    .get(url, {params})
-    .then((response) => {
-      dispatch(fetchContactsSuccess(response));
-    })
-    .catch((error) => {
-      dispatch(fetchContactsFailure(error.message));
-    });
+      .get(url, { params })
+      .then((response) => {
+        dispatch(fetchContactsSuccess(response));
+      })
+      .catch((error) => {
+        dispatch(fetchContactsFailure(error.message));
+      });
   };
 };
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,42 +1,61 @@
-import './Modal.scss'
+import "./Modal.scss";
 
-import React, { useEffect, useState, useRef } from 'react';
-import { connect, useSelector } from 'react-redux';
-import Scrollbar  from 'react-custom-scrollbars';
+import React, { useEffect, useState } from "react";
+import { connect, useSelector } from "react-redux";
+import Scrollbar from "react-custom-scrollbars";
 
+import Button from "../Button/Button";
+import {
+  openModal,
+  closeModal,
+  fetchContacts,
+  toggleCheckbox,
+} from "../../actions/index";
+import { getFilteredProducts } from "../../selectors/modalSelectors";
 
-import Button from  '../Button/Button'
-import { openModal, closeModal, fetchContacts, toggleCheckbox } from '../../actions/index'
-import { getFilteredProducts } from '../../selectors/modalSelectors';
-
-const Modal = ({show, closeModal, openModal, fetchContacts, toggleCheckbox}) => {
-  const checkboxChecked = useSelector((state) => state.contactReducer.checkBoxChecked);
+const Modal = ({
+  show,
+  closeModal,
+  openModal,
+  fetchContacts,
+  toggleCheckbox,
+}) => {
+  const checkboxChecked = useSelector(
+    (state) => state.contactReducer.checkBoxChecked
+  );
   const filteredProducts = useSelector(getFilteredProducts);
-  const modalType = useSelector((state) => state.modal.modalType)
+  const modalType = useSelector((state) => state.modal.modalType);
 
   const [page, setPage] = useState(1);
-  const [products, setProducts] = useState([])
-  const [searchQuery, setSearchQuery] = useState('');
+  const [products, setProducts] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
 
   let debounceTimeout;
 
-  useEffect(() => { fetchContacts('', page, modalType) }, [page, modalType]);
+  useEffect(() => {
+    fetchContacts("", page, modalType);
+  }, [page, modalType, fetchContacts]);
+
   useEffect(() => {
     if (filteredProducts) {
-      let newProducts = filteredProducts
+      let newProducts = filteredProducts;
 
-      if(searchQuery == '' && checkboxChecked == false) {
+      if (searchQuery === "" && checkboxChecked === false) {
         newProducts = filteredProducts.filter((newProduct) => {
-          return !products.some((existingProduct) => existingProduct.id === newProduct.id);
+          return !products.some(
+            (existingProduct) => existingProduct.id === newProduct.id
+          );
         });
 
         setProducts([...products, ...newProducts]);
-      } else { setProducts(newProducts) }
+      } else {
+        setProducts(newProducts);
+      }
     }
   }, [filteredProducts, checkboxChecked]);
 
   const handleScroll = (e) => {
-    if(searchQuery == '') {
+    if (searchQuery === "") {
       const { scrollTop, scrollHeight, clientHeight } = e.target;
       if (scrollHeight - scrollTop === clientHeight) {
         setPage(page + 1);
@@ -44,18 +63,22 @@ const Modal = ({show, closeModal, openModal, fetchContacts, toggleCheckbox}) => 
     }
   };
 
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(() => {
+      fetchContacts(searchQuery, page, modalType);
+    }, 500);
+    return () => clearTimeout(delayDebounceFn);
+  }, [searchQuery]);
+
   const handleInputChange = (e) => {
     var query = e.target.value;
-    setSearchQuery(query);
 
     clearTimeout(debounceTimeout);
 
-    if (e.key === 'Enter') {
+    if (e.key === "Enter") {
       fetchContacts(query, page, modalType);
     } else {
-      debounceTimeout = setTimeout(() => {
-        fetchContacts(query, page, modalType);
-      }, 300);
+      setSearchQuery(query);
     }
   };
 
@@ -63,60 +86,67 @@ const Modal = ({show, closeModal, openModal, fetchContacts, toggleCheckbox}) => 
     setPage(1);
     setProducts([]);
     openModal(modalType);
-  }
+  };
 
   return (
-      <div className={`${show ? 'show' : ''} `} tabIndex="-1" role="dialog">
-        <div className="modal-dialog" role="document">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h5 className="modal-title">Modal {modalType}</h5>
-            </div>
-            <div className="modal-body">
+    <div className={`${show ? "show" : ""} `} tabIndex="-1" role="dialog">
+      <div className="modal-dialog" role="document">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title">Modal {modalType}</h5>
+          </div>
+          <div className="modal-body">
+            <input
+              type="text"
+              placeholder="Search..."
+              className="form-control"
+              onChange={handleInputChange}
+              onKeyDown={handleInputChange}
+            />
+            <br />
+            <Scrollbar onScroll={handleScroll}>
+              <ul className="contact-list" style={{ listStyle: "none" }}>
+                {products?.map((product) => (
+                  <li key={product.id}>
+                    <b>Title: </b>
+                    {product.title} <br />
+                    <b>Price:</b> {product.price}
+                    <br />
+                    <br />
+                  </li>
+                ))}
+              </ul>
+            </Scrollbar>
+          </div>
+          <div className="modal-footer">
+            <div className="form-check">
               <input
-                type="text"
-                placeholder="Search..."
-                className="form-control"
-                onChange={handleInputChange}
-                onKeyDown={handleInputChange}
-              /> <br/>
-              <Scrollbar
-                onScroll={handleScroll}
-              >
-                <ul
-                  className='contact-list'
-                  style={{ listStyle: 'none' }}
-                >
-                  {products?.map((product) => (
-                    <li key={product.id}>
-                    <b>Title: </b>{product.title} <br/>
-                    <b>Price:</b> {product.price}<br /><br/>
-                    </li>
-                  ))}
-                </ul>
-              </Scrollbar>
+                className="form-check-input"
+                type="checkbox"
+                value=""
+                id="flexCheckDefault"
+                checked={checkboxChecked}
+                onChange={() => toggleCheckbox(!checkboxChecked)}
+              />
+              <label className="form-check-label" htmlFor="flexCheckDefault">
+                Only Even
+              </label>
             </div>
-            <div className='modal-footer'>
-              <div className='form-check'>
-                <input
-                  className='form-check-input'
-                  type="checkbox"
-                  value=""
-                  id="flexCheckDefault"
-                  checked={checkboxChecked}
-                  onChange={() => toggleCheckbox(!checkboxChecked)}
-                />
-                <label className='form-check-label' htmlFor="flexCheckDefault">
-                  Only Even
-                </label>
-              </div>
-              <Button text='All Products' variant='a' onClick={() => changeModal('A')} />
-              <Button text='Laptops' variant='b' onClick={() => changeModal('B')} />
-              <Button text='Close' variant='c' onClick={closeModal} />
-            </div>
+            <Button
+              text="All Products"
+              variant="a"
+              onClick={() => changeModal("A")}
+            />
+            <Button
+              text="Laptops"
+              variant="b"
+              onClick={() => changeModal("B")}
+            />
+            <Button text="Close" variant="c" onClick={closeModal} />
           </div>
         </div>
       </div>
+    </div>
   );
 };
 
@@ -132,8 +162,8 @@ const mapDispatchToProps = (dispatch) => {
       dispatch(closeModal());
     },
     toggleCheckbox: (checkBoxChecked) => {
-      dispatch(toggleCheckbox(checkBoxChecked))
-    }
+      dispatch(toggleCheckbox(checkBoxChecked));
+    },
   };
 };
 
